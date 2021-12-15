cb8eae8410ddad7dea87181ded480b87216dd04d
Oggy107 <urmalsingh10@gmail.com>
2021-12-15 17:08:35 +0530

    Made nft data dynamic

--- src/components/PunkList.js
+++ src/components/PunkList.js
@@ -1,23 +1,50 @@
 import React from 'react'
 
 import ethLogo from '../assets/eth.png'
+import loadingAnimation from '../assets/loading.gif'
 
 const PunkList = () => {
+    const [punkListData, setPunkListData] = React.useState([])
+    const [loading, setLoading] = React.useState(true);
+
+    React.useEffect(async () => {
+        const resp = await fetch('https://testnets-api.opensea.io/assets?asset_contract_address=0x295E39F8A3E61690CbbD4aCda9f067Bf72B79F99&order_direction=asc')
+        const openseaData = await resp.json()
+        setPunkListData(openseaData.assets)
+        console.log(openseaData.assets)
+        setLoading(false)
+    }, [])
+
+    if (loading)
+    {
+        return (
+            <div className="punkList-container">
+                <img src={loadingAnimation} alt="" />
+            </div>
+        )
+    }
+
     return (
         <div className='punkList-container'>
-            <div className="punkCard-container">
-                <div className="punkImg-container">
-                    <img src="https://static01.nyt.com/images/2021/03/12/arts/11nft-auction-cryptopunks-print/11nft-auction-cryptopunks-print-articleLarge.jpg?quality=75&auto=webp&disable=upscale" alt="" />
-                </div>
-                <div className="punkDetail-container">
-                    <h2 className='punkName'>cool punk</h2>
-                    <p className="punkId">.#1</p>
-                    <div className="price-container">
-                        <img src={ethLogo} alt="" />
-                        <p>5</p>
+            {
+                punkListData.map((item, index) => {
+                    return (
+                    <div key={index} className="punkCard-container">
+                        <div className="punkImg-container">
+                            <img src={item.image_url} alt="" />
+                        </div>
+                        <div className="punkDetail-container">
+                            <h2 className='punkName'>{item.name}</h2>
+                            <p className="punkId">.#{item.token_id}</p>
+                            <div className="price-container">
+                                <img src={ethLogo} alt="" />
+                                <p>{item.traits[0].value}</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+                    )
+                })
+            }
         </div>
     )
 }
